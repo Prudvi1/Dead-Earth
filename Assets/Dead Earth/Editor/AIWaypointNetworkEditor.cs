029146d333b50937e0ac606310630300c6035df2
Prudhvi Raj <prudviboga@gmail.com>
2018-11-01 16:59:19 -0400

    Custom Editor
    Script to design custom editor and find paths between points using navmesh

---
+++ Assets/Dead Earth/Editor/AIWaypointNetworkEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+[CustomEditor(typeof(AIWaypointNetwork))]
+public class AIWaypointNetworkEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        AIWaypointNetwork network = (AIWaypointNetwork)target;
+
+        network.DispayMode = (PathDisplayMode)EditorGUILayout.EnumPopup("Display Mode", network.DispayMode);
+
+        if (network.DispayMode == PathDisplayMode.Paths)
+        {
+            network.UIStart = EditorGUILayout.IntSlider("Waypoint Start", network.UIStart, 0, network.Waypoints.Count - 1);
+            network.UIEnd = EditorGUILayout.IntSlider("Waypoint End", network.UIEnd, 0, network.Waypoints.Count - 1);
+
+        }
+        
+        DrawDefaultInspector();
+    }
+
+    private void OnSceneGUI()
+    {
+        AIWaypointNetwork network = (AIWaypointNetwork)target;
+        for(int i = 0; i < network.Waypoints.Count; i++)
+        {
+            Handles.color = Color.yellow;
+            Handles.Label(network.Waypoints[i].position, "Waypoint "+i.ToString());
+    
+        }
+
+        if (network.DispayMode == PathDisplayMode.Connections)
+        {
+            Vector3[] linePoints = new Vector3[network.Waypoints.Count + 1];
+
+            for (int i = 0; i <= network.Waypoints.Count; i++)
+            {
+                int index = i != network.Waypoints.Count ? i : 0;
+                if (network.Waypoints[index] != null)
+                    linePoints[i] = network.Waypoints[index].position;
+                else
+                    linePoints[i] = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+            }
+            //linePoints[network.Waypoints.Count] = network.Waypoints[0].position;
+            Handles.color = Color.cyan;
+            Handles.DrawPolyLine(linePoints);
+        }
+        else if (network.DispayMode == PathDisplayMode.Paths)
+        {
+            NavMeshPath path = new NavMeshPath();
+
+            if (network.Waypoints[network.UIStart]!=null && network.Waypoints[network.UIEnd] != null)
+            {
+                Vector3 from = network.Waypoints[network.UIStart].position;
+                Vector3 to = network.Waypoints[network.UIEnd].position;
+
+                NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+                Handles.color = Color.yellow;
+                Handles.DrawPolyLine(path.corners);
+            }
+ 
+        }
+        
+    }
+}
--- Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
+++ Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
+
+public enum PathDisplayMode { None, Connections, Paths};
 
 public class AIWaypointNetwork : MonoBehaviour {
+    [HideInInspector]
+    public PathDisplayMode DispayMode = PathDisplayMode.Connections;
+    [HideInInspector]
+    public int UIStart = 0;
+    [HideInInspector]
+    public int UIEnd = 0;
+
     public List<Transform> Waypoints = new List<Transform>();
 
 }
